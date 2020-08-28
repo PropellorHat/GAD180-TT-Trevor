2c37cfc1798c09f72e55215192ea97cfb76ea99c
puufx <51160851+puufx@users.noreply.github.com>
2020-08-28 09:35:36 +0800

    MeleeUpdated

---
+++ Assets/Scripts/Noah/MeleeNPC.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class MeleeNPC : MonoBehaviour
+{
+  /*  private float TimeBetwAttack;
+    public float startTimeBetwAttack;
+
+    public Transform attackPos;
+    public float attackRange;
+    public LayerMask whatIsEnemies;
+
+    void Update()
+    {
+        if (TimeBetwAttack <= 0)
+        {
+            //then u can atk
+            if(Input.GetKey(KeyCode.Space))
+            {
+                Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange);
+
+                for (int i = 0; i < enemiesToDamage.Length; i++)
+                {
+                    enemiesToDamage[i].GetComponent<Enemy>().TakeDamage(damage);
+                }
+                //TimeBetwAttack = startTimeBetwAttack;
+            }
+
+            TimeBetwAttack = startTimeBetwAttack;
+        }
+        else
+        {
+            TimeBetwAttack -= Time.DeltaTime;
+        }
+
+    }
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        Debug.Log("Damage Taken!");
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPos.position, attackRange);
+    }
+
+
+
+    */
+
+}
---
+++ Assets/Scripts/Noah/MeleeRange.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+
+public class MeleeRange : MonoBehaviour
+{
+
+
+    public float TargetRange = 3f;
+    private Rigidbody2D rb;
+
+    public Animator animator;
+    public float dist;
+    public GameObject Target;
+    private Transform player;
+
+
+
+    public float damage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        player = GameObject.FindWithTag("Player").GetComponent<Transform>();
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        dist = Vector3.Distance(player.position, Target.transform.position);
+        if (dist < 2.0f)
+        {
+            Attack();
+        }
+        /* if (Input.GetKeyDown(KeyCode.Space))
+             {
+                 Attack();
+                 Debug.Log(" space is pressed");
+             }
+        
+
+             if (trigger.activeInHierarchy == true)
+                {
+                    Attack();
+                }
+       */
+
+
+
+
+
+    }
+
+
+
+
+    public void Attack()
+    {
+        animator.SetTrigger("attack");
+        Debug.Log("Melee initiated");
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player") //if it hits enemy npc
+        {
+            collision.gameObject.GetComponent<Health>().TakeDamage(damage);
+            
+        }
+    }
+}
